07bf7c6e5524c3885294ac92e5b220f85b09b8c5
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-08-24 18:53:29 +0200

    Add elementary classes and logging; reconsider arguments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import os
 import argparse
+import logging
 
 import basebot
 import instabot
@@ -10,12 +11,48 @@ import instabot
 INSTANT_ROOM_TEMPLATE = os.environ.get('INSTANT_ROOM_TEMPLATE',
                                        'wss://instant.leet.nu/room/{}/ws')
 
+class EuphoriaSendBot(basebot.HeimEndpoint):
+    pass
+
+class InstantSendBot(instabot.Bot):
+    pass
+
+class InstantBotManager(basebot.BotManager):
+    def make_bot(self, roomname=Ellipsis, passcode=Ellipsis,
+                 nickname=Ellipsis, logger=Ellipsis, **config):
+        if passcode is not Ellipsis:
+            raise TypeError('Instant bots do not have passcodes')
+        if logger in (None, Ellipsis):
+            logger = None
+        else:
+            raise TypeError('Instant sending bots do not have loggers')
+        if roomname is not Ellipsis:
+            config['url'] = INSTANT_ROOM_TEMPLATE.format(roomname)
+            roomname = Ellipsis
+        return basebot.BotManager.make_bot(self, Ellipsis, Ellipsis,
+                                           nickname, logger, **config)
+
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument('--euphoria-room', required=True, metavar='ROOMNAME',
-                   help='Euphoria room to bridge')
-    p.add_argument('--instant-room', required=True, metavar='ROOMNAME',
-                   help='Instant room to bridge')
+    p.add_argument('--loglevel', default='INFO',
+                   help='Logging level to use')
+    p.add_argument('--euphoria-room', metavar='ROOMNAME', default='test',
+                   help='Euphoria room to bridge (default &test)')
+    p.add_argument('--instant-room', metavar='ROOMNAME', default='test',
+                   help='Instant room to bridge (default &test)')
     arguments = p.parse_args()
+    loglevel = arguments.loglevel
+    logging.basicConfig(format='[%(asctime)s %(name)s %(levelname)s] '
+        '%(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=loglevel)
+    mgr = basebot.BotManager(botcls=EuphoriaSendBot,
+                             botname='EuphoriaBridge')
+    mgr.add_child(InstantBotManager(botcls=InstantSendBot,
+                                    botname='InstantBridge'))
+    try:
+        mgr.main()
+    except (KeyboardInterrupt, SystemExit) as exc:
+        mgr.shutdown()
+        mgr.join()
+        if isinstance(exc, SystemExit): raise
 
 if __name__ == '__main__': main()
